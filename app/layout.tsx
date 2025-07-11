17bd83094f4f5bb42c32090606ff8916e573eda4
R0dn3 <u19305755@utp.edu.pe>
2025-07-11 01:04:30 -0500

    configuration inside the app/layout.tsx folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ClientLayout from "@/components/ClientLayout"; 
 
-const font= Poppins({
-  
+const font = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    })       
-/*const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});*/
 
 export const metadata: Metadata = {
   title: "Nice For You",
@@ -24,15 +15,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
-        {children}
+      <body className={`${font.className} antialiased`}>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
